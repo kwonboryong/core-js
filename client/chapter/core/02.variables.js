cad0b8e3b90788f93ec23ef19f03efcddf730e0c
kwonboryong <kwonboryong@naver.com>
2024-05-30 13:21:53 +0900

    js

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,34 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+let admin, name; // 변수 두 개를 동시에 선언할 수 있습니다.
+name = 'John';
+admin = name;
+alert(admin); // "John"
+
+const ourPlanet = 'Earth';
+let userNameOnline = 'name';
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+let priceByQuantity = 20000;
+// - 구매 제품 가격의 총 합
+let total = 35000;
+// - 1년 기준 일(day)자 수
+const YEARDATE = 365;
+// - 구매 결제 여부
+let isPayment = true;
+let hasCartList = false;
+// - 구매 결제 내역
+let paymentList;
+// - 브랜드 접두사
+const BRAND_PREFIX = 'Nike';
+// - 오늘의 운세
+let todaysFortune = 'good';
+// - 상품 정보
+const productInfo = '샴푸';
+
+/* variables -------------------------------------------------- */
+
+/* constant variables -------------------------------------------------- */
---
+++ client/chapter/core/06.dataTypes.js
@@ -0,0 +1,113 @@
+/* ------------------------ */
+/* Data Types               */
+/* ------------------------ */
+
+/* ECMAScript의 8가지 데이터 타입 -------------------------------------------- */
+
+// 1. 존재하지 않는(nothing) 값 / 비어있는(empty) 값 / 알 수 없는(unknown) 값
+let empty = null;
+console.log(typeof empty);
+
+// 2. 값이 할당되지 않은 상태
+let undef;
+console.log(typeof undef);
+
+// 3. 따옴표를 사용해 묶은 텍스트(큰", 작은', 역`)
+const double = 'hello';
+const single = 'hello';
+const backtick = `hello ${10 + 5}`;
+
+console.log(typeof backtick);
+
+const str = new String('hello');
+// String 생성자 함수를 사용하여 문자열 객체를 만든다
+
+// 4. 정수, 부동 소수점 숫자(길이 제약)
+const integer = 150; // number literal
+const floatingPointNumber = 10.5;
+
+console.log(typeof integer);
+console.log(typeof floatingPointNumber);
+
+const num = new Number(150);
+console.log(num);
+
+// 5. 길이에 제약이 없는 정수(예: 암호 관련 작업에서 사용)
+const bigInt = 123n;
+console.log(typeof bigInt);
+
+// 6. 참(true, yes) 또는 거짓(false, no)
+const isActive = true;
+console.log(typeof isActive);
+
+// 7. 데이터 컬렉션(collection) 또는 복잡한 엔티티(entity)
+const obj = {
+  name: 'tiger',
+  aaa: 'bbb',
+  bbb: 'ccc',
+};
+console.log(obj);
+
+const object = new Object({ name: 'lion' }); // 생성자 함수(constructor function)
+
+console.log(object);
+
+// 8. 고유한 식별자(unique identifier)
+// 생성자 함수로만 만들 수 있음
+const id = Symbol('uuid');
+const id2 = Symbol('uuid');
+
+console.log(typeof id);
+
+/* typeof 연산자의 2가지 사용법 ---------------------------------------------- */
+
+// 1) 연산자 typeof
+// 2) 함수 typeof()
+
+// 언어 상, 오류
+
+console.clear();
+
+// Object
+const user = {
+  name: 'tiger',
+  age: 20,
+};
+
+console.log(user.age);
+
+// Array
+const newArray = new Array(1, 2, 3); // 생성자 함수
+
+const array = new Array(2); // 길이가 2인 빈 배열이 생김
+
+const arr = [
+  10,
+  100,
+  1000,
+  null,
+  undefined,
+  'hello',
+  { name: 'tiger' },
+  function () {},
+];
+console.log(arr[0]);
+
+// function
+function plus(a, b) {
+  return a + b;
+}
+
+plus(1, 2);
+
+function 붕어빵틀(재료) {
+  return `따끈 따끈 맛있는 ${재료} 맛 붕어빵`;
+}
+
+const 팥붕 = 붕어빵틀('팥');
+const 슈붕 = 붕어빵틀('슈크림');
+const 와붕 = 붕어빵틀('와사비');
+
+붕어빵틀(팥붕, 슈붕, 와붕);
+
+// this

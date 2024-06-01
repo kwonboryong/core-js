ad62899818d6f9d762037e08ada040076f8a7b4d
kwonboryong <kwonboryong@naver.com>
2024-06-01 20:42:26 +0900

    js

--- client/chapter/core/09-1.condition.js
+++ client/chapter/core/09-1.condition.js
@@ -2,48 +2,67 @@
 /* Condition        */
 /* ---------------- */
 
-// const result = prompt("자바스크립트의 '공식'이름은 무엇일까요?");
+// const result = prompt('자바스크립트의 "공식" 이름은 무엇일까요?','');
 
-// if (result === "ECMAScript") {
-//   alert('정답입니다!');
-// } else {
-//   alert('모르셨나요? 정답은 ECMAScript입니다!');
+// if(result === 'ECMAScript'){
+//   console.log('정답입니다!');
+// }else{
+//   console.log('모르셨나요? 정답은 ECMAScript 입니다!');
 // }
 
-// 그 영화 봤니?
-//     ↓
-// Yes | No
-//     | 영화 볼거니?
-//           ↓
-//       Yes | No
-
-// 영화 봤니?
-
-// if 문(statement)
-// else 절(clause)
-// else if 복수 조건 처리
 function watchingMovie() {
-  let didWatchMovie = confirm('그 영화 봤니?');
+  // 그 영화 봤니?
+  //     ↓
+  // Yes | No
+  //     | 영화 볼거니?
+  //           ↓
+  //       Yes | No
+
+  // 영화 봤니?
+  let didWatchMovie = confirm('듄2 영화 봤어?');
 
   if (didWatchMovie) {
-    alert('영화 어땠니?');
+    console.log('그 영화 참 재밌더라!');
   } else {
-    let goingToWatchMovie = confirm('그 영화 볼거니?');
+    let goingToWatchMovie = confirm('영화 볼거니?');
 
     if (goingToWatchMovie) {
-      alert('같이 보자');
+      // 영화 볼거야
+      let withWho = prompt('누구랑 볼거니??');
+
+      if (withWho === 'you') {
+        console.log('사랑해');
+      } else {
+        console.log('왜 나랑 안봐요?');
+      }
     } else {
-      alert('왜 안보니?');
+      // 안볼거야
+      console.log('그래 나중에 한번 꼭 봐!');
     }
   }
+
+  // 영화 볼거니?
 }
 
+// if 문(statement)
+
+// else 절(clause)
+
+// else if 복수 조건 처리
 
 // 조건부 연산자
+
 let didWatchMovie = 'no';
 let goingToWatchMovie = 'yes';
 
-const result = didWatchMovie === 'yes' ? '어땠어?' : '같이 볼래?';
+const message = didWatchMovie.includes('yes')
+  ? '그 영화 참 재밌더라'
+  : goingToWatchMovie.includes('yes')
+    ? '언제 볼까? 재밌겠다'
+    : '그래..';
+
+// 멀티 조건부 연산자 식
+
 
 
 function render(node,isActive){
@@ -53,5 +72,3 @@ function render(node,isActive){
   `
   node.insertAdjacentHTML('beforeend',template);
 }
-
-// 멀티 조건부 연산자 식

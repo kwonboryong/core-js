7c75814dafd8a06364497a1d69ce2ed16f30f9c6
kwonboryong <kwonboryong@naver.com>
2024-06-26 15:58:45 +0900

    0626

--- client/main.js
+++ client/main.js
@@ -1,9 +1,3 @@
-import { xhrPromise } from "./lib/index.js";
+import { } from "./lib/index.js";
 
-async function getData() {
-  const data = await xhrPromise.get('https://jsonplaceholder.typicode.com/users')
-
-  console.log(data);
-}
-
-getData()
+const ENDPOINT = 'https://jsonplaceholder.typicode.com/users'
